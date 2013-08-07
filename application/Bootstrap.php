495f781f50a61a6bf1799062caed6edfe6e8e9d3
root <root@wildmax-2>
2013-08-07 16:59:26 +0400

    1) изменил шаблон 2) установил Blueprint

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,18 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    protected function _initViewHelpers(){
+       error_reporting(E_ALL);
+        ini_set("display_errors", 1);        
+       $this->bootstrap('layout');
+        $layout = $this->getResource('layout');
+        $view = $layout->getView();
+        
+        $view->headMeta()->appendHttpEquiv('Content-Type','text/html;charset=utf-8');
+        $view->headTitle('SimpleBlog');
+        $view->headTitle()->setSeparator(' :: ');        
+    }
+            
 
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,16 +5,18 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-         $qw = zend_version();
-         echo $qw;
+       
+       //  $qw = zend_version();
+         //echo $qw;
         /* Initialize action controller here */
     }
 
     public function indexAction()
     {
-        // action body
-         $qw = zend_version();
-         echo $qw;
+            // action body
+     //    $qw = zend_version();
+       //  echo $qw;
+        $this->view->headTitle('Главная','PREPEND');
     }
 
 
